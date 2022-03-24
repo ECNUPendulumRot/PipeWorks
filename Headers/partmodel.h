9e3aef9ff8eaa5a089e277f1fca9bc9d1693785f
Sherman-in-D <1973671061@qq.com>
2022-03-24 13:37:28 +0800

    finish

--- Headers/partmodel.h
+++ Headers/partmodel.h
@@ -144,26 +144,6 @@ private:
                                          {7,    Parameter("carSPEED"       , QStringLiteral("小车速度"), false)},
                                          {8,    Parameter("TargetCur"      , QStringLiteral("干伸高度值"), true)},
                                      });
-
-//    static inline QHash<QString, QString> engToChn =
-//            QHash<QString, QString>({
-//                                        {"angle",               "角度"},
-//                                        {"stayTime_Lead",       "前枪边停时间"},
-//                                        {"stayTime_Trail",      "后枪边停时间"},
-//                                        {"oscWidth_Lead",       "前枪摆宽"},
-//                                        {"oscWidth_Trail",      "后枪摆宽"},
-//                                        {"oscFreq_Lead",        "前枪摆动周期"},
-//                                        {"oscFreq_Trail",       "后枪摆动周期"},
-//                                        {"feedRate_Lead",       "前枪送丝速度"},
-//                                        {"feedRate_Trail",      "后枪送丝速度"},
-//                                        {"Arc_Rate_Lead",       "前枪电弧修正"},
-//                                        {"Arc_Rate_Trail",      "后枪电弧修正"},
-//                                        {"leadTargetCur",       "前枪垂向目标值"},
-//                                        {"trailTargetCur",      "后枪垂向目标值"},
-//                                        {"carACC",              "小车加速度"},
-//                                        {"carSPEED",            "小车速度"},
-
-//                                    });
 };
 
 #endif // PARTMODEL_H
